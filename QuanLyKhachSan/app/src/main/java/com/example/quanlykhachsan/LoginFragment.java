800556ff5e32b4d45c65e91af30bc308b0896ac2
HuyZa2911 <huyza.2911@gmail.com>
2020-07-21 14:59:34 +0700

    dialog_booking

--- QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/LoginFragment.java
+++ QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/LoginFragment.java
@@ -1,16 +1,38 @@
 package com.example.quanlykhachsan;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class LoginFragment extends AppCompatActivity {
+    Button btnLogin;
+    Intent intent;
+    EditText etAcount, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.login_fragment);
-//        
+        btnLogin = findViewById(R.id.btnLogin);
+        btnClickLogin();
+
+// Write a message to the database
+
+    }
+
+    private void btnClickLogin() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(LoginFragment.this, TestDialog.class);
+                startActivity(intent);
+            }
+        });
     }
 }
