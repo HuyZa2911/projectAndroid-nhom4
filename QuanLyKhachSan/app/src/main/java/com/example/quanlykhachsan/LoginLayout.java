70c5dd22b399b8bf9c2e06b8a7c346c8d7d4290b
Huy Bùi <56296845+HuyZa2911@users.noreply.github.com>
2020-07-24 14:33:34 +0700

    Update LoginLayout.java

--- QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/LoginLayout.java
+++ QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/LoginLayout.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class LoginLayout extends AppCompatActivity {
+public class LoginFragment extends AppCompatActivity {
     Button btnLogin;
     Intent intent;
     EditText etAcount, etPassword;
@@ -18,16 +18,22 @@ public class LoginLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.control_hotel_background);
+//      btnLogin = findViewById(R.id.btnLogin);
+//        btnClickLogin();
 
+// Write a message to the database
 
+    }
 
     private void btnClickLogin() {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               
+                intent = new Intent(LoginFragment.this, TestDialog.class);
+                startActivity(intent);
             }
         });
+
     }
 }
