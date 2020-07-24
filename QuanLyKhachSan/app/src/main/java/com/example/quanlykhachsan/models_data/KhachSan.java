465cd458e875690d48a0f5f0ff526b53215a975b
cobedoimatbuon0908 <50957818+cobedoimatbuon0908@users.noreply.github.com>
2020-07-24 13:41:20 +0700

    Update model_data và models_manage_data_

--- QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/models_data/KhachSan.java
+++ QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/models_data/KhachSan.java
@@ -1,47 +1,15 @@
 package com.example.quanlykhachsan.models_data;
 
+import com.example.quanlykhachsan.models_manage_data.ListPhong;
+
 import java.util.ArrayList;
 
 public class KhachSan {
     private int idKhachSan;
-    private ArrayList<Phong> arrayListPhong;
+
     private String tenKS;
     private String diaChi;
     private int loaiKS;
-
-    public void addPhong(Phong phong) {
-        arrayListPhong.add(phong);
-    }
-
-    public void updatePhong(Phong phong) {
-        for (Phong phong1 : arrayListPhong) {
-            if (phong1.getIdPhong() == phong.getIdPhong()) {
-                phong1.setGiaPhong(phong.getGiaPhong());
-                phong1.setLoaiPhong(phong.getLoaiPhong());
-                phong1.setSoGiuong(phong.getSoGiuong());
-                phong1.setTrangThai(phong.getTrangThai());
-                break;
-            }
-
-        }
-
-    }
-    public void deletePhong(Phong phong){
-        for (Phong phong1 : arrayListPhong) {
-            if (phong1.getIdPhong() == phong.getIdPhong()) {
-                arrayListPhong.remove(phong1);
-                break;
-            }
-
-        }
-    }
-    public KhachSan(int idKhachSan, ArrayList<Phong> arrayListPhong, String tenKS, String diaChi, int loaiKS) {
-        this.idKhachSan = idKhachSan;
-        this.arrayListPhong = arrayListPhong;
-        this.tenKS = tenKS;
-        this.diaChi = diaChi;
-        this.loaiKS = loaiKS;
-    }
 
     public int getIdKhachSan() {
         return idKhachSan;
@@ -49,15 +17,6 @@ public class KhachSan {
 
     public void setIdKhachSan(int idKhachSan) {
         this.idKhachSan = idKhachSan;
-    }
-
-    public ArrayList<Phong> getArrayListPhong() {
-        return arrayListPhong;
-    }
-
-    public void setArrayListPhong(Phong... phong) {
-        for (Phong phong1 : phong)
-            this.arrayListPhong.add(phong1);
     }
 
     public String getTenKS() {
@@ -81,6 +40,13 @@ public class KhachSan {
     }
 
     public void setLoaiKS(int loaiKS) {
+        this.loaiKS = loaiKS;
+    }
+
+    public KhachSan(int idKhachSan, String tenKS, String diaChi, int loaiKS) {
+        this.idKhachSan = idKhachSan;
+        this.tenKS = tenKS;
+        this.diaChi = diaChi;
         this.loaiKS = loaiKS;
     }
 }
--- QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/models_data/Phong.java
+++ QuanLyKhachSan/app/src/main/java/com/example/quanlykhachsan/models_data/Phong.java
@@ -2,10 +2,48 @@ package com.example.quanlykhachsan.models_data;
 
 public class Phong {
     private int idPhong;
+    private int idKhachSan;
     private int soGiuong;
     private int loaiPhong;
     private double giaPhong;
     private int trangThai;
+    private Double giaTienTheoGio;
+    private Double getGiaTienTheoNgay;
+
+    public Double getGiaTienTheoGio() {
+        return giaTienTheoGio;
+    }
+
+    public void setGiaTienTheoGio(Double giaTienTheoGio) {
+        this.giaTienTheoGio = giaTienTheoGio;
+    }
+
+    public Double getGetGiaTienTheoNgay() {
+        return getGiaTienTheoNgay;
+    }
+
+    public void setGetGiaTienTheoNgay(Double getGiaTienTheoNgay) {
+        this.getGiaTienTheoNgay = getGiaTienTheoNgay;
+    }
+
+    public Phong(int idPhong, int idKhachSan, int soGiuong, int loaiPhong, double giaPhong, int trangThai, Double giaTienTheoGio, Double getGiaTienTheoNgay) {
+        this.idPhong = idPhong;
+        this.idKhachSan = idKhachSan;
+        this.soGiuong = soGiuong;
+        this.loaiPhong = loaiPhong;
+        this.giaPhong = giaPhong;
+        this.trangThai = trangThai;
+        this.giaTienTheoGio = giaTienTheoGio;
+        this.getGiaTienTheoNgay = getGiaTienTheoNgay;
+    }
+
+    public int getIdKhachSan() {
+        return idKhachSan;
+    }
+
+    public void setIdKhachSan(int idKhachSan) {
+        this.idKhachSan = idKhachSan;
+    }
 
     public void setIdPhong(int idPhong) {
         this.idPhong = idPhong;
@@ -50,11 +88,4 @@ public class Phong {
     public Phong() {
     }
 
-    public Phong(int idPhong, int soGiuong, int loaiPhong, double giaPhong, int trangThai) {
-        this.idPhong = idPhong;
-        this.soGiuong = soGiuong;
-        this.loaiPhong = loaiPhong;
-        this.giaPhong = giaPhong;
-        this.trangThai = trangThai;
-    }
 }
